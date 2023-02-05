492a307cf6e19abfeccf81223da27c6c6d8da0af
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-02-05 20:16:13 +0100

    walking pepe

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,27 @@
 class Character extends MovableObject {
+  images_walking = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.images_walking);
+
+    this.animate();
   }
+
+  animate() {
+    setInterval(() => {
+      let path = this.images_walking[this.currentImage];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 1000);
+  }
+
   jump() {}
 }
