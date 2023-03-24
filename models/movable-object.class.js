4a20d38339ef5794d9f72e7238ecf72578583720
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-03-24 22:38:03 +0100

    02 | 09 instanceOf

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -34,11 +34,17 @@ class MovableObject {
   }
 
   drawFrame(ctx) {
-    ctx.beginPath();
-    ctx.lineWidth = "2";
-    ctx.strokeStyle = "blue";
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+    if (
+      this instanceof Character ||
+      this instanceof Chicken ||
+      this instanceof Endboss
+    ) {
+      ctx.beginPath();
+      ctx.lineWidth = "2";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
   }
 
   loadImages(arr) {
