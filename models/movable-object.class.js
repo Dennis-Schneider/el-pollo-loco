9b166eb267b89f090f596228dca04541051c8963
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-03-26 11:12:34 +0200

    02 | 18 Statusbar IV

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,14 +1,7 @@
-class MovableObject {
-  x = 120;
-  y = 55;
-  img;
-  height = 280;
-  width = 150;
+class MovableObject extends DrawableObject {
   speed = 0.2;
   speedY = 0;
   acceleration = 2;
-  imageCache = [];
-  currentImage = 0;
   otherDirection = false;
   energy = 100;
   lastHit = 0;
@@ -24,29 +17,6 @@ class MovableObject {
 
   isAboveGround() {
     return this.y <= 150;
-  }
-
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-
-  drawFrame(ctx) {
-    if (
-      this instanceof Character ||
-      this instanceof Chicken ||
-      this instanceof Endboss
-    ) {
-      ctx.beginPath();
-      ctx.lineWidth = "2";
-      ctx.strokeStyle = "blue";
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
-    }
   }
 
   isColliding(mo) {
@@ -85,14 +55,6 @@ class MovableObject {
 
   isDead() {
     return this.energy == 0;
-  }
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
   }
 
   playAnimation(images) {
--- models/world.class.js
+++ models/world.class.js
@@ -1,11 +1,11 @@
 class World {
   character = new Character();
   level = level1;
-
   canvas;
   ctx;
   keyboard;
   camera_x = 0;
+  statusBar = new StatusBar();
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -40,6 +40,7 @@ class World {
     this.ctx.translate(this.camera_x, 0);
 
     this.addObjectsToMap(this.level.backgroundObjects);
+    this.addToMap(this.statusBar);
     this.addObjectsToMap(this.level.clouds);
     this.addObjectsToMap(this.level.enemies);
     this.addToMap(this.character);
