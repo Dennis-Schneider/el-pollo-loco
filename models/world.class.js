89a9e70117888b71ffaebe583cac31c8e6bfff52
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-03-27 22:08:44 +0200

    bottleBar function & throwableObjects

--- models/world.class.js
+++ models/world.class.js
@@ -9,6 +9,7 @@ class World {
   bottleBar = new BottleBar();
   coinBar = new CoinBar();
   throwableObjects = [];
+  collectedBottle = [];
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -25,18 +26,49 @@ class World {
 
   run() {
     setInterval(() => {
+      this.checkCollectionBottles();
       this.checkCollisions();
-      this.checkThrowObjects();
+      this.checkThrowObject();
     }, 200);
   }
 
-  checkThrowObjects() {
-    if (this.keyboard.D) {
-      let bottle = new ThrowableObject(
+  checkCollectionBottles() {
+    this.level.bottle.forEach((bottle) => {
+      if (this.character.isColliding(bottle)) {
+        this.bottleIsCollected();
+        this.character.collectBottle();
+        this.collectedBottle = this.collectedBottle.splice(0, 4);
+        this.collectedBottle.push(bottle);
+        this.bottleBar.setPercentage(this.character.bottle);
+      }
+    });
+    if (this.bottleBar.percentage == 100) {
+    }
+  }
+
+  bottleIsCollected() {
+    if (this.collectedBottle.length < 5) {
+      for (let i = 0; i < this.level.bottle.length; i++) {
+        const bottle = this.level.bottle[i];
+        if (this.character.isColliding(bottle)) {
+          // this.bottleCollectSound();
+          this.level.bottle.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  checkThrowObject() {
+    if (this.keyboard.D && this.collectedBottle.length > 0) {
+      let collectedBottle = new ThrowableObject(
         this.character.x + 100,
         this.character.y + 100
       );
-      this.throwableObjects.push(bottle);
+      this.character.bottle -= 20;
+      this.bottleBar.setPercentage(this.character.bottle);
+      this.throwableObjects = this.throwableObjects.splice(0, 1);
+      this.throwableObjects.push(collectedBottle);
+      this.collectedBottle.splice(0, 1);
     }
   }
 
