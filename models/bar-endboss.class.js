765d3a94b00e903f77987367446d3617464c17d3
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-05 20:28:35 +0200

    statusbar endboss

--- models/bar-endboss.class.js
+++ models/bar-endboss.class.js
@@ -1,11 +1,11 @@
 class StatusBarEndboss extends DrawableObject {
   images = [
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
-    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
+    "img/7_statusbars/2_statusbar_endboss/health/0.png",
+    "img/7_statusbars/2_statusbar_endboss/health/20.png",
+    "img/7_statusbars/2_statusbar_endboss/health/40.png",
+    "img/7_statusbars/2_statusbar_endboss/health/60.png",
+    "img/7_statusbars/2_statusbar_endboss/health/80.png",
+    "img/7_statusbars/2_statusbar_endboss/health/100.png",
   ];
 
   percentage = 100;
@@ -14,9 +14,9 @@ class StatusBarEndboss extends DrawableObject {
     super();
     this.loadImages(this.images);
     this.x = 500;
-    this.y = 0;
+    this.y = 15;
     this.width = 200;
-    this.height = 54;
+    this.height = 34;
     this.setPercentage(100);
   }
 
