e77f83cd0dbde53d830e40e48bfc0d7c3a7d1a92
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-09 19:42:17 +0200

    mobile detection & mobile screen size

--- js/game.js
+++ js/game.js
@@ -4,20 +4,16 @@ let world;
 let keyboard = new Keyboard();
 
 function startGame() {
-  // display("loading-screen");
-  // setTimeout(() => {
   level1 = createLevel1(world);
   initWorld();
-  // }, 2500);
-  // setTimeout(() => {
   hide("start-screen");
-  //   hide("loading-screen");
-  // }, 3000);
+  checkForMobileStartGame();
 }
 
 function initWorld() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
+  checkForMobile();
 }
 
 function clearAllIntervals() {
@@ -55,12 +51,67 @@ function display(id) {
 
 function win() {
   showScreen("win-screen");
+  hide("game-controls-mobile");
   clearAllIntervals();
 }
 
 function gameOver() {
   showScreen("game-over-screen");
+  hide("game-controls-mobile");
   clearAllIntervals();
+}
+
+function fullscreen() {
+  let fullscreen = document.getElementById("canva-section");
+  enterFullscreen(fullscreen);
+}
+
+function enterFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    // for IE11 (remove June 15, 2022)
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    // iOS Safari
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+window.addEventListener("resize", function (event) {
+  checkForMobile();
+});
+
+function checkForMobile() {
+  const isMobile = navigator.userAgentData.mobile;
+  let cs = document.getElementById("canva-section");
+  let canvas = document.getElementById("canvas");
+  if (isMobile) {
+    cs.style.borderRadius = "0";
+    cs.style.height = "100vh";
+    canvas.style.width = "100vw";
+  } else {
+    cs.style.borderRadius = "25px";
+    cs.style.height = "unset";
+    canvas.style.width = "unset";
+  }
+}
+
+function checkForMobileStartGame() {
+  const isMobile = navigator.userAgentData.mobile;
+  if (isMobile) {
+    display("game-controls-mobile");
+  } else {
+    hide("game-controls-mobile");
+  }
 }
 
 window.addEventListener("keydown", (event) => {
