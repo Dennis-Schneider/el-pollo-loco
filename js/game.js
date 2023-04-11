b4f26d592bcf78d2d3c238c898a1166c6bcb4ee7
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-11 20:42:24 +0200

    jsDoc game.js

--- js/game.js
+++ js/game.js
@@ -13,25 +13,38 @@ let walkingSound = new Audio("./audio/walking.mp3");
 let ouchSound = new Audio("./audio/ouch.mp3");
 let gameOverSound = new Audio("./audio/gameover.mp3");
 
+/**
+ * The function starts the game by creating level 1, initializing the world, hiding the start screen,
+ * and playing background music.
+ */
 function startGame() {
   level1 = createLevel1(world);
   initWorld();
   hide("start-screen");
   playSound(backgroundMusic, 0.05);
-
-  // checkForMobileStartGame();
 }
 
+/**
+ * The function initializes a world object with a canvas and keyboard input.
+ */
 function initWorld() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
-  // checkForMobile();
 }
 
+/**
+ * The above code contains two functions, one to clear all intervals and another to toggle the
+ * visibility of an HTML element.
+ */
 function clearAllIntervals() {
   for (let i = 1; i < 9999; i++) window.clearInterval(i);
 }
 
+/**
+ * The function toggles the visibility of an HTML element by adding or removing the "d-none" class.
+ * @param id - The id parameter is a string that represents the id attribute of the HTML element that
+ * we want to toggle the visibility of.
+ */
 function toggleVisibility(id) {
   let element = document.getElementById(id);
   if (element.classList.contains("d-none")) {
@@ -41,11 +54,22 @@ function toggleVisibility(id) {
   }
 }
 
+/**
+ * The function plays a sound with a specified volume.
+ * @param sound - The sound parameter is a variable that represents an audio file or object that can be
+ * played using JavaScript. It could be an HTML5 audio element or an instance of the Audio class in
+ * JavaScript.
+ * @param volume - The volume parameter is a number that represents the volume level of the sound. It
+ * can range from 0 (silent) to 1 (maximum volume).
+ */
 function playSound(sound, volume) {
   sound.play();
   sound.volume = volume;
 }
 
+/**
+ * The function toggles the sound on and off and changes the sound icon accordingly.
+ */
 function toggleSounds() {
   let soundImg = document.getElementById("sound-image");
   if (soundImg.src.includes("_on")) {
@@ -57,10 +81,20 @@ function toggleSounds() {
   }
 }
 
+/**
+ * The function stops a given sound from playing.
+ * @param sound - The sound parameter is a variable that represents an audio element in HTML. It is
+ * used in the function to pause the audio playback.
+ */
 function stopSound(sound) {
   sound.pause();
 }
 
+/**
+ * The function toggles the mute status of multiple sounds.
+ * @param status - a boolean value that determines whether to mute or unmute all sounds. If status is
+ * true, all sounds will be muted. If status is false, all sounds will be unmuted.
+ */
 function toggleAllSounds(status) {
   backgroundMusic.muted = status;
   chickenSound.muted = status;
@@ -72,6 +106,9 @@ function toggleAllSounds(status) {
   gameOverSound.muted = status;
 }
 
+/**
+ * The function stops all sounds in a game.
+ */
 function stopAllSounds() {
   stopSound(backgroundMusic);
   stopSound(chickenSound);
@@ -80,10 +117,18 @@ function stopAllSounds() {
   stopSound(ouchSound);
 }
 
+/**
+ * The function restarts the game by showing the start screen.
+ */
 function restartGame() {
   showScreen("start-screen");
 }
 
+/**
+ * The function hides all screens except for the one specified by the "name" parameter.
+ * @param name - The name of the screen that needs to be displayed. It could be "loading-screen",
+ * "start-screen", "game-over-screen", or "win-screen".
+ */
 function showScreen(name) {
   hide("loading-screen");
   hide("start-screen");
@@ -92,14 +137,30 @@ function showScreen(name) {
   display(name);
 }
 
+/**
+ * The function adds the "d-none" class to an HTML element with a specified ID to hide it.
+ * @param id - The parameter "id" is a string that represents the ID of an HTML element that we want to
+ * hide. The function uses the document.getElementById() method to select the element with the
+ * specified ID and then adds the "d-none" class to it using the classList.add() method. This class is
+ */
 function hide(id) {
   document.getElementById(id).classList.add("d-none");
 }
 
+/**
+ * The function removes the "d-none" class from an HTML element with a specified ID, making it visible.
+ * @param id - The parameter "id" is a string that represents the ID of an HTML element that we want to
+ * display. The function "display" uses the getElementById method to select the element with the
+ * specified ID and then removes the "d-none" class from its classList. This class is often used to
+ */
 function display(id) {
   document.getElementById(id).classList.remove("d-none");
 }
 
+/**
+ * The function "win" mutes the walking sound, shows the win screen, hides the game controls for
+ * mobile, clears all intervals, and stops all sounds.
+ */
 function win() {
   walkingSound.muted = true;
   showScreen("win-screen");
@@ -108,6 +169,10 @@ function win() {
   stopAllSounds();
 }
 
+/**
+ * The function "gameOver" stops all sounds, shows the game over screen, hides the game controls for
+ * mobile, and clears all intervals.
+ */
 function gameOver() {
   playSound(gameOverSound, 0.5);
   walkingSound.muted = true;
@@ -116,62 +181,3 @@ function gameOver() {
   hide("game-controls-mobile");
   clearAllIntervals();
 }
-
-function fullscreen() {
-  let fullscreen = document.getElementById("canva-section");
-  enterFullscreen(fullscreen);
-}
-
-function enterFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.msRequestFullscreen) {
-    // for IE11 (remove June 15, 2022)
-    element.msRequestFullscreen();
-  } else if (element.webkitRequestFullscreen) {
-    // iOS Safari
-    element.webkitRequestFullscreen();
-  }
-}
-
-function exitFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  }
-}
-
-// toggle Sound
-
-// window.addEventListener("resize", function (event) {
-//   checkForMobile();
-// });
-
-// function checkForMobile() {
-//   const isMobile = navigator.userAgentData.mobile;
-//   let cs = document.getElementById("canva-section");
-//   let canvas = document.getElementById("canvas");
-//   if (isMobile) {
-//     cs.style.borderRadius = "0";
-//     cs.style.height = "100vh";
-//     cs.style.width = "100vw";
-//   } else {
-//     cs.style.borderRadius = "25px";
-//     cs.style.height = "unset";
-//     cs.style.width = "unset";
-//   }
-// }
-
-// function checkForMobileStartGame() {
-//   const isMobile = navigator.userAgentData.mobile;
-//   if (
-//     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-//       navigator.userAgent
-//     )
-//   ) {
-//     display("game-controls-mobile");
-//   } else {
-//     hide("game-controls-mobile");
-//   }
-// }
